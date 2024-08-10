eddf59aa19af7b2e96f0ff99500dceb729e19dac
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2024-08-10 15:18:35 +0530

    Create The New Line - Java.java

---
+++ The New Line - Java.java
@@ -0,0 +1,8 @@
+static void printIndividualLine(){
+        
+        // Your code here
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+        
+    }

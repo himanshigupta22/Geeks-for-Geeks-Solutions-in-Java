0949f28396442e93961fa6c4c090163bb83472a3
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2024-08-11 23:14:30 +0530

    Create Data Types - Java.java

---
+++ Data Types - Java.java
@@ -0,0 +1,15 @@
+class Geeks{
+    
+    
+    static void dataTypes(int a, float b, double c, long l, byte d){
+        
+        double p = c/b;//c/b
+        double q = b/a;//b/a
+        double r = c/a;//c/a
+        double m = r+l;//r+l
+        int s = a/d;//a/d
+      
+        System.out.println(p + " " + q + " " + r + " " + m + " " + s);
+        
+    }
+}

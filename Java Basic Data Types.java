65d2faaa52b631041e6b4e80b7bd2e4a125ce742
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2024-08-12 23:22:53 +0530

    Create Java Basic Data Types.java

---
+++ Java Basic Data Types.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    int javaIntType(Scanner sc) {
+        // code here
+     
+        int num = sc.nextInt();
+        return num;
+    }
+    
+    String javaStringType(Scanner sc) {
+        // code here
+        sc.nextLine();
+        String st = sc.nextLine();
+        return st;
+    }
+    
+    float javaFloatType(Scanner sc) {
+        // code here
+      
+        float fl = sc.nextFloat();
+        return fl;
+    }
+};

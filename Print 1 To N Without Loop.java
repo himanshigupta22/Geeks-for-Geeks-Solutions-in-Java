c0e0a8fe196c9e4e332a9278c73d0fbdf8c9dc73
Himanshi Gupta <146530350+himanshigupta22@users.noreply.github.com>
2024-08-15 17:51:24 +0530

    Create Print 1 To N Without Loop.java

---
+++ Print 1 To N Without Loop.java
@@ -0,0 +1,11 @@
+class Solution
+{
+    
+  public void printNos(int N)
+    {
+        //Your code here
+        for(int idx=1; idx<=N; idx++){
+            System.out.print(idx+" ");
+        }
+    }
+}
